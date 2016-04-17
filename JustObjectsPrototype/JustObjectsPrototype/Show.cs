3bbb6cf134e9ad05051729ed4d4d222d6d039e2a
Manuel Naujoks <mnaujoks@gmail.com>
2016-04-17 21:26:39 +0200

    xmldoc

--- JustObjectsPrototype/JustObjectsPrototype/Show.cs
+++ JustObjectsPrototype/JustObjectsPrototype/Show.cs
@@ -1,4 +1,5 @@
 ﻿using JustObjectsPrototype.UI;
+using JustObjectsPrototype.UI.Editors;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -6,16 +7,32 @@ using System.Linq;
 
 namespace JustObjectsPrototype
 {
+	/// <summary>
+	/// Setup the prototype with .For() or .ViewOf().
+	/// Invoke the prototype with .With(objects).
+	/// </summary>
 	public static class Show
 	{
-		public static Prototype With(params IEnumerable<object>[] objects)
+		/// <summary>
+		/// Start setup for type without limiting displayed types.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
+		public static PrototypeBuilder<T> For<T>()
 		{
-			return new PrototypeBuilder().With(objects);
+			return new PrototypeBuilder<T>
+			{
+				Settings = new Settings
+				{
+					DisplayedTypes = new List<Type> { }
+				}
+			};
 		}
-		public static Prototype With(ICollection<object> objects)
-		{
-			return new PrototypeBuilder().With(objects);
-		}
+		/// <summary>
+		/// Start setup for type and limit displayed types to T.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
 		public static PrototypeBuilder<T> ViewOf<T>()
 		{
 			return new PrototypeBuilder<T>
@@ -26,6 +43,11 @@ namespace JustObjectsPrototype
 				}
 			};
 		}
+		/// <summary>
+		/// Show prototype with no objects and limit displayed types to T.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
 		public static Prototype EmptyViewOf<T>()
 		{
 			return new PrototypeBuilder<T>
@@ -36,7 +58,24 @@ namespace JustObjectsPrototype
 				}
 			}.With(new List<object>());
 		}
-
+		/// <summary>
+		/// Show prototype with objects from all passed in object collections without limiting displayed types.
+		/// </summary>
+		/// <param name="objects"></param>
+		/// <returns></returns>
+		public static Prototype With(params IEnumerable<object>[] objects)
+		{
+			return new PrototypeBuilder().With(objects);
+		}
+		/// <summary>
+		/// Show prototype with objects from the passed in object collection without limiting displayed types.
+		/// </summary>
+		/// <param name="objects"></param>
+		/// <returns></returns>
+		public static Prototype With(ICollection<object> objects)
+		{
+			return new PrototypeBuilder().With(objects);
+		}
 
 		public class PrototypeBuilder
 		{
@@ -47,6 +86,11 @@ namespace JustObjectsPrototype
 
 			internal Settings Settings { get; set; }
 
+			/// <summary>
+			/// Complete setup and show prototype with objects from all passed in object collections.
+			/// </summary>
+			/// <param name="objects"></param>
+			/// <returns></returns>
 			public Prototype With(params IEnumerable<object>[] objects)
 			{
 				if (objects.Length == 0 || objects.Any(o => o == null)) throw new ArgumentNullException();
@@ -54,6 +98,11 @@ namespace JustObjectsPrototype
 				var collection = new ObservableCollection<object>(objects.Aggregate((o1, o2) => o1.Concat(o2)));
 				return With(collection); ;
 			}
+			/// <summary>
+			/// Complete setup and show prototype with objects from the passed in object collection.
+			/// </summary>
+			/// <param name="objects"></param>
+			/// <returns></returns>
 			public Prototype With(ICollection<object> objects)
 			{
 				if (objects.Any(o => o == null)) throw new ArgumentNullException();
@@ -85,6 +134,23 @@ namespace JustObjectsPrototype
 			{
 			}
 
+			/// <summary>
+			/// Continue setup for new type without limiting displayed types.
+			/// </summary>
+			/// <typeparam name="TNext"></typeparam>
+			/// <returns></returns>
+			public PrototypeBuilder<TNext> For<TNext>()
+			{
+				return new PrototypeBuilder<TNext>
+				{
+					Settings = Settings
+				};
+			}
+			/// <summary>
+			/// Continue setup for new type and limit displayed types to TNext as well.
+			/// </summary>
+			/// <typeparam name="TNext"></typeparam>
+			/// <returns></returns>
 			public PrototypeBuilder<TNext> ViewOf<TNext>()
 			{
 				Settings.DisplayedTypes.Add(typeof(TNext));
@@ -94,31 +160,60 @@ namespace JustObjectsPrototype
 					Settings = Settings
 				};
 			}
+			/// <summary>
+			/// Setup to not support object creation via ribbon.
+			/// </summary>
+			/// <returns></returns>
 			public PrototypeBuilder<T> DisableNew()
 			{
 				Settings.AllowNew[typeof(T)] = false;
 				return this;
 			}
-			public PrototypeBuilder<T> EnableNew(Action<T> afterNew)
+			/// <summary>
+			/// Setup to support object creation via ribbon.
+			/// </summary>
+			/// <param name="afterNew">Callback to extend object creation.</param>
+			/// <returns></returns>
+			public PrototypeBuilder<T> EnableNew(Action<T> afterNew = null)
 			{
 				Settings.AllowNew[typeof(T)] = true;
-				Settings.NewEvents[typeof(T)] = new Action<object>(o => afterNew((T)o));
+				if (afterNew != null)
+				{
+					Settings.NewEvents[typeof(T)] = new Action<object>(o => afterNew((T)o));
+				}
 				return this;
 			}
+			/// <summary>
+			/// Setup to not support object deletion via ribbon.
+			/// </summary>
+			/// <returns></returns>
 			public PrototypeBuilder<T> DisableDelete()
 			{
 				Settings.AllowDelete[typeof(T)] = false;
 				return this;
 			}
-			public PrototypeBuilder<T> EnableDelete(Action<T> afterDelete)
+			/// <summary>
+			/// Setup to support object deletion via ribbon.
+			/// </summary>
+			/// <param name="afterDelete">Callback to extend object deletion.</param>
+			/// <returns></returns>
+			public PrototypeBuilder<T> EnableDelete(Action<T> afterDelete = null)
 			{
 				Settings.AllowDelete[typeof(T)] = true;
 				Settings.DeleteEvents[typeof(T)] = new Action<object>(o => afterDelete((T)o));
 				return this;
 			}
+			/// <summary>
+			/// Extend object change via detail view.
+			/// </summary>
+			/// <param name="afterValueChanged">Callback to extend object change.</param>
+			/// <returns></returns>
 			public PrototypeBuilder<T> OnValueChanged(Action<T> afterValueChanged)
 			{
-				Settings.ChangeEvents[typeof(T)] = new Action<object>(o => afterValueChanged((T)o));
+				Settings.ChangeEvents[typeof(T)] = new Action<ObjectChangedEventArgs>(o =>
+				{
+					afterValueChanged((T)o.Object);
+				});
 				return this;
 			}
 		}
